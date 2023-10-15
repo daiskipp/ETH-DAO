a9890a8415b84ee9c697930321eefa94f96feae5
Daisuke Maeda <daiskipp@gmail.com>
2023-10-15 12:26:28 +0900

    upload to github

---
+++ src/scripts/module.ts
@@ -0,0 +1,4 @@
+export const editionDropAddress = '0x59dDfC0f854bab66aAE84E65F56B2AcF30514f07';
+export const ERCTokenAddress = '0x64Bb60261468DC3A45DFA1F76Cf28557B7d29B3E';
+export const gavananceAddress = '0xcBF47D21BcF4761D559D8eC2fB1D8cB3C80A52F3';
+export const ownerWalletAddress = '0x586994276F16F09F3499d48debc0E063a495514e';
