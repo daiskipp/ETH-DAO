9fcf05e557d2b78876f9b5f2c7f36b0071081782
Daisuke Maeda <daiskipp@gmail.com>
2023-11-05 20:30:58 +0900

    fix

--- src/scripts/5-deploy-token.ts
+++ src/scripts/5-deploy-token.ts
@@ -7,9 +7,9 @@ import sdk from './1-initialize-sdk.js';
     // 標準的なERC-20のコントラクトをデプロイする
     const tokenAddress = await sdk.deployer.deployToken({
       // トークン名 Ex. 'Ethereum'
-      name: 'Tokyo Sauna Collective Governance Token',
+      name: 'Tokyo Sauna Collective Governance Token2',
       // トークンシンボル Ex. 'ETH'
-      symbol: 'TSC',
+      symbol: 'TSC2',
       // これは、トークンを売却する場合の受け取り先の設定
       // 今回は販売しないので、再び AddressZero に設定
       primary_sale_recipient: AddressZero,
