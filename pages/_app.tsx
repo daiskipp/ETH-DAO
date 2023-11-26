14e5c89b6343c600fb9ad34a367e58afbfdc07e1
dicekey <daiskipp@gmail.com>
2023-11-26 17:30:57 +0900

    thirdweb clientid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import { Sepolia } from '@thirdweb-dev/chains';
 import {ThirdwebProvider } from '@thirdweb-dev/react';
 import type { AppProps } from 'next/app';
-
 import HeadComponent from '../components/head';
 import '../styles/globals.css';
 
+const clientid = process.env.THIRDWEB_CLIENT_ID;
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={Sepolia}>
+    <ThirdwebProvider activeChain={Sepolia} clientId={clientid}>
       <HeadComponent/>
       <Component {...pageProps} />
     </ThirdwebProvider>
