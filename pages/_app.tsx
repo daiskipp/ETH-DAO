8cad1508549e2286e6e41f1e18acda21c5025236
dicekey <daiskipp@gmail.com>
2023-11-27 00:57:54 +0900

    env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
+import type { AppProps } from "next/app";
 import { Sepolia } from '@thirdweb-dev/chains';
 import {ThirdwebProvider } from '@thirdweb-dev/react';
 import type { AppProps } from 'next/app';
 import HeadComponent from '../components/head';
 import '../styles/globals.css';
 
-const clientid = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={Sepolia} clientId={clientid}>
+    <ThirdwebProvider activeChain={Sepolia} clientId={process.env.NEXT_PUBLIC_CLIENT_ID}>
       <HeadComponent/>
       <Component {...pageProps} />
     </ThirdwebProvider>
